4b2d6c4a3210ac2ee0bd45b63114e14d0b7e626f
Ania <annalangner06@gmail.com>
2019-06-11 14:10:57 +0200

    Adding a few other endpoints

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.get('/', function (req, res) {
     res.send('Hello GET!');
 });
 
+app.get('/list_user', function (req, res) {
+    console.log('I received a GET request to the /list_user page');
+    res.send('Website with a list of users!');
+});
+app.get('/ab*cd', function(req, res) {
+    console.log('I received a GET request to the /ab*cd page');
+    res.send('The pattern fits');
+});
+
 app.post('/', function (req, res) {
     console.log('I received a POST request to the home page');
     res.send('Hello POST!');
