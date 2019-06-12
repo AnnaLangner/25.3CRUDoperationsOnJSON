4a711cb13c5372af4228efa6e6feb944a3a2d981
Ania <annalangner06@gmail.com>
2019-06-12 11:15:24 +0200

    modification of endpoints and updating the package.json file

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 var express = require('express');
 var fs = require('fs');
+bodyParser = require('body-parser');
 var app = express();
 var stringifyFile = '';
 
 app.use(bodyParser.json());
 
 app.get('/getNote', function (req, res) {
-    res.send(
-        fs.readFile('./package.json', 'utf8', function (err, data) {
-            if(err) throw err;
-            stringifyFile = data;
-            res.send(data);
-        })
-    )
+
+    fs.readFile('./package.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        stringifyFile = data;
+        res.send(data);
+    })
+
 });
 
 app.post('/updateNote/:note', function (req, res) {
-    res.send(
-        fs.writeFile('.package.json', stringifyFile, function (err) {
-            if (err) throw err;
-            console.log('file updated');
-        })
-    )
+    fs.writeFile('.package.json', stringifyFile, function (err) {
+        if (err) throw err;
+        console.log('file updated');
+    })
 });
 
 app.listen(3000);
