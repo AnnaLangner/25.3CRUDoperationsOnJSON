240b9719acee4e3d07453f09b06b514d29b32c25
Ania <annalangner06@gmail.com>
2019-06-12 10:45:28 +0200

    Creating the POST endpoint

--- server.js
+++ server.js
@@ -9,8 +9,19 @@ app.get('/getNote', function (req, res) {
     res.send(
         fs.readFile('./package.json', 'utf8', function (err, data) {
             if(err) throw err;
-            stringifyFile = data
+            stringifyFile = data;
             res.send(data);
         })
     )
-})
+});
+
+app.post('/updateNote/:note', function (req, res) {
+    res.send(
+        fs.writeFile('.package.json', stringifyFile, function (err) {
+            if (err) throw err;
+            console.log('file updated');
+        })
+    )
+});
+
+app.listen(3000);
