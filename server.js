ed84ad0b12e7af9579e015246ce57fd17c9f2704
Ania <annalangner06@gmail.com>
2019-06-12 10:38:37 +0200

    Assigning dependencies to variables in the server.js file and creating the GET endpoint

--- server.js
+++ server.js
@@ -1,33 +1,16 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express();
+var stringifyFile = '';
 
-app.get('/', function (req, res) {
-    console.log('I received a GET request to the home page');
-    res.send('Hellow World!');
-});
+app.use(bodyParser.json());
 
-// app.get('/list_user', function (req, res) {
-//     console.log('I received a GET request to the /list_user page');
-//     res.send('Website with a list of users!');
-// });
-// app.get('/ab*cd', function(req, res) {
-//     console.log('I received a GET request to the /ab*cd page');
-//     res.send('The pattern fits');
-// });
-
-app.post('/', function (req, res) {
-    console.log('I received a POST request to the home page');
-    res.send('Hello POST!');
-});
-
-app.delete('/del_user', function (req, res) {
-    console.log('I received a DELETE request to the /del/user page');
-    res.send('Hello DELETE!');
-});
-
-app.listen(3000);
-
-app.use(function (req, res, next) {
-    res.status(404).send('Sorry, we could not find what you want!')
-});
+app.get('/getNote', function (req, res) {
+    res.send(
+        fs.readFile('./package.json', 'utf8', function (err, data) {
+            if(err) throw err;
+            stringifyFile = data
+            res.send(data);
+        })
+    )
+})
