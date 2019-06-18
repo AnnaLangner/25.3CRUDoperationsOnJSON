de8db04d6b20c263e8d4afbb83bcf16adb51deee
Anna Langner <annalangner06@gmail.com>
2019-06-18 11:52:42 +0200

    The last corrections server.js file

--- server.js
+++ server.js
@@ -5,24 +5,26 @@ var app = express();
 var stringifyFile = '';
 
 app.use(bodyParser.json());
-
-app.get('/getNote', function (req, res) {
-
+app.use('/', function(req, res, next) {
     fs.readFile('./package.json', 'utf8', function (err, data) {
         if (err) throw err;
         stringifyFile = data;
-        res.send(data);
+        next();
     })
+})
 
+app.get('/getNote', function (req, res) {
+    res.send(stringifyFile);
 });
 
 app.post('/updateNote/:note', function (req, res) {
-    req.params.note = stringifyFile;
-    fs.writeFile('.package.json', stringifyFile, function (err) {
+
+    stringifyFile += req.params.note;
+    fs.writeFile('./package.json', stringifyFile, function (err) {
         if (err) throw err;
-        console.log('file updated');
         res.send(stringifyFile);
     })
+
 });
 
 app.listen(3000);
